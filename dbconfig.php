2d5e46eb960c3e312529adc3dff5749b192f406d
xhenetahasani <xhenetaa.hasani@gmail.com>
2020-05-17 03:45:26 -1000

    Delete

--- dbconfig.php
+++ dbconfig.php
@@ -1,9 +1,9 @@
 <?php
+//qitu jon parametrat qe hyjn ne DB per koneksion me databaz
 
 define('DBHOST', 'localhost');
 define('DBNAME', 'managmentsystem');
 define('DBUSER', 'root');
-define('DBPASS', '');
-
+define('DBPASS', 'vitiakademik');
 
 ?>
