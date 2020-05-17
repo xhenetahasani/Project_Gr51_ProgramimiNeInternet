805b483240316059069d085bd484611954d8a268
xhenetahasani <xhenetaa.hasani@gmail.com>
2020-05-17 04:13:04 -1000

    Create index.php

---
+++ index.php
@@ -0,0 +1,36 @@
+<?php
+
+require_once "autoinclude.php";
+
+if (isset($_SERVER['PATH_INFO'])) {
+    $path = $_SERVER['PATH_INFO'];
+    $pathSplit = explode('/', ltrim($path, '/'));
+} else {
+    $pathSplit = '/';
+}
+
+if ($pathSplit === '/') {
+
+    $controllerObj = new Controllers\DefaultController();
+
+    $method = "index";
+
+    print $controllerObj->$method();
+
+
+} else {
+    $controllerName = "Controllers\\" . $pathSplit[0] . "Controller";
+    if(array_key_exists(1,$pathSplit)){
+        if ($pathSplit[1] != "") {
+            $req_method = "$pathSplit[1]";
+        }
+    }else{
+        $req_method='index';
+    }
+
+    $controllerObj = new $controllerName();
+
+    print $controllerObj->$req_method();
+
+}
+?>
