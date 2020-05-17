a7f079acff02753ffcce94936493ed2a391dd952
xhenetahasani <xhenetaa.hasani@gmail.com>
2020-05-17 05:29:06 -1000

    Create DefaultController.php

---
+++ Controllers/DefaultController.php
@@ -0,0 +1,11 @@
+<?php
+namespace Controllers;
+
+class DefaultController{
+    public function index() {
+        session_start();
+        session_destroy();
+        return \ViewHelper::render("login");
+    }
+}
+?>
